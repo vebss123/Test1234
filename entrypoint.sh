d40fc1044cbf6728ee872021b75417e855bdca29
vaibhavwanare <vaibhavwanare007@gmail.com>
2023-07-10 12:19:00 +0530

    adding docker ,licence,action.yml and entrypoint.sh file

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ue
+
+RepositoryName="${INPUT_REPOSITORY_NAME}"
+AwsRegion="${INPUT_AWS_REGION}"
+CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
+
+git config --global --add safe.directory /github/workspace
+git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+git remote add sync ${CodeCommitUrl}
+git push sync --mirror
